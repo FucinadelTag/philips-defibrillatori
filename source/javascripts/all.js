ee81fdef03e92bd758139465228d13ec9eeeca8b
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2014-07-30 16:09:46 +0200

    Concluso cambio testata

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,12 @@
 //= require jquery/dist/jquery
 //= require foundation/js/foundation.min
-
-//= require app
+//= require jquery.cookie
 //= require script
 //= require purl
 //= require prismic.io-1.0.12.min
-//= require jquery.cookie
+
+//= require app
+
+
+
+//= require manageHeader
---
+++ source/javascripts/manageHeader.js
@@ -0,0 +1,106 @@
+var Configuration = {
+
+  // -- API endpoint
+  apiEndpoint: 'https://philips-defibrillatori.prismic.io/api',
+}
+
+$.cookie.json = true;
+
+function manageHeader () {
+
+    var headerData = {};
+    headerData['default'] = {};
+
+    var paramOk = 'default';
+
+    var setParam = function ()
+    {
+        url = $.url();
+        param = url.param('t');
+
+        if (param)
+        {
+            paramOk = param
+            $.cookie('target', param, { expires: 60 });
+            return;
+        }
+
+        if  ($.cookie('target'))
+        {
+            paramOk = ($.cookie('target'));
+            return;
+        }
+
+    }
+
+    var initPrismicApi = function ()
+    {
+        Prismic.Api(Configuration.apiEndpoint, doQuery);
+    }
+
+    var doQuery = function (err, Api)
+    {
+        Api.form('everything').ref(Api.master()).query('[[:d = fulltext(my.testate-pagine.target, "'+ paramOk +'")]]').submit(getData);
+
+    }
+
+    var getData = function (err, documents)
+    {
+        if (err) { console.log(err); return; }
+
+        if (documents.results.length > 0)
+        {
+            headerData[paramOk] = {
+                prima_linea: documents.results[0].getText('testate-pagine.prima-linea'),
+                seconda_linea: documents.results[0].getText('testate-pagine.seconda-linea'),
+                image_testata: documents.results[0].getImageView('testate-pagine.image', 'wide').url
+            };
+
+            changeHeader (headerData[paramOk]);
+
+
+
+        }
+
+        $.cookie('headerData', headerData, { expires: 30 });
+
+    }
+
+    var changeHeader = function (data) {
+
+        $('#prima-linea').html(data['prima_linea']);
+        $('#seconda-linea').html(data['seconda_linea']);
+        $('#image-testata').attr("src", data['image_testata']);
+    }
+
+    var manageData = function ()
+    {
+
+        headerDataCookie = $.cookie('headerData');
+
+        if (headerDataCookie)
+        {
+            headerData = headerDataCookie;
+        }
+
+        if(headerData[paramOk])
+        {
+            changeHeader (headerData[paramOk]);
+        }
+        else
+        {
+            Api = initPrismicApi ();
+        }
+
+    }
+
+    this.doManage = function ()
+    {
+        setParam ();
+
+        manageData ();
+
+        //console.log ($.cookie('headerData'));
+
+    };
+}
